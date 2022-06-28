d1b4176929e70891f421373120c1338ce2f36f92
Andreea Goidea <32072420+arndreea@users.noreply.github.com>
2022-06-28 08:55:35 +0300

    Add files via upload

---
+++ BloodConnect-API/src/main/java/HelloSpring.java
@@ -0,0 +1,11 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+public class HelloSpring {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpring.class, args);
+    }
+}
